4d93a53883acede4f37ee4edf8d8cf98cee92bdc
Rajath Kiran A <arajath5463@gmail.com>
2025-08-10 16:33:37 +0530

    Added the WEEK-2 Programs Inside the Source-code directory and the Respective outputs inside the Output Directory

---
+++ Source-code/WEEK-2/Print Statements.py
@@ -0,0 +1,9 @@
+
+# Here We are learning about the Print statements in Python
+
+n = input("Who are you?, Enter your name?\n")
+print("Hello", n)
+print("Hello", n, "I hope your life is going well")
+
+m = input("Want to chat with me?")
+print(m, "Ya i got your respone you seems to be intrested in chating with me haa?")
---
+++ Source-code/WEEK-2/Understanding variables.py
@@ -0,0 +1,15 @@
+# Variable Assignment
+t1 = "Naveen"
+t2 = "Rithesh"
+t3 = "sharath"
+
+# Printing Variables
+print("Hello", t1)
+print("Hello", t2)
+print("Hello", t3)
+
+# Variable Overwriting
+t1 = "Naveen Kumar"
+print(t1)
+t1 = "India"
+print(t1)
