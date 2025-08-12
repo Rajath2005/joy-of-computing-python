af5e4f57bbb28299ed6e982c311d1e49094c64e0
Rajath Kiran A <arajath5463@gmail.com>
2025-08-12 20:18:18 +0530

    Added program to the week-3 and the output image also

---
+++ Source-code/WEEK-3/lists.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 12 20:01:17 2025
+
+@author: Rajath
+"""
+##################### LISTS ###################################
+
+# Here we are going to learn about the usage of the lists in python.
+
+#We created a list  beelow.
+shopping=["Coffee","Sugar","Milk"]
+
+#Using for loops to print the items present in the list.
+for item in shopping:
+    print(item) # printing the items present.
+    
+print("\n")    
+#we can print it in other way
+
+print(shopping)
+
+# we can add items to list by using append .
+
+shopping.append("Curd")
+
+#Printing the list directly after the appending adding one more values to it.
+print(shopping)
