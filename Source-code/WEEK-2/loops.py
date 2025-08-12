e5bd5b243e096ae6dd4f5486a951cf6e392e5ec4
Rajath Kiran A <arajath5463@gmail.com>
2025-08-12 18:11:13 +0530

    Added new program and respective output of it

---
+++ Source-code/WEEK-2/loops.py
@@ -0,0 +1,14 @@
+# A basic "for" loop to print a statement multiple times
+for i in range(10):
+    print("Hi, how are you?")
+
+# The "range" function generates numbers from 0 to 9 (10 times).
+
+# Using a "for" loop with a variable to print numbers and their multiples
+answer = 0
+for i in range(5):
+    answer += i
+    print("For I in range 5: answer =", answer)
+
+# The loop adds numbers from 0 to 4 to the "answer" variable.
+
