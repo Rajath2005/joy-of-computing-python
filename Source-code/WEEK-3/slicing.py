5747e9e0f31b73a9e210758985f0515877b90e20
Rajath Kiran A <arajath5463@gmail.com>
2025-08-13 20:04:01 +0530

    Added new Program

---
+++ Source-code/WEEK-3/slicing.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 13 19:49:39 2025
+
+@author: araja
+"""
+
+print("\n\nNames Printing:")
+
+# Here we are creating one list contain the names of the students
+
+students=["Rajath","Kiran","Sheethal","Rithesh","Sanath","Sharath","Sudeep"]
+
+# Slicing
+
+#List_names[start:end+1]
+
+#List_names[start_index:end_index+1]
+
+print(students[1:5])
+
+# It has a default value also
+
+print(students[:])
+
+# Default start index=0
+
+#default end index=len(list)
+
+print(students[2:5])
+
