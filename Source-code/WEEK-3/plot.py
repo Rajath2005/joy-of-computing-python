207fb0e278b587f71bbd4df3fee324da5a6489b4
Rajath2005 <arajath5463gmail.com>
2025-08-28 10:31:19 +0530

    Updated the Plot.py

---
+++ Source-code/WEEK-3/plot.py
@@ -0,0 +1,14 @@
+#Created on  Aug 28
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],[1,4,9,16],'g^')
+
+#For dot in grpah we are  using "ro"
+
+#To print in the doted line we can use "r--"
+
+#To print in the blue squares we can use "bs"
+
+#To print in the green triangles we can use "bs"
+
+plt.ylabel('some numbers')
+plt.show()
